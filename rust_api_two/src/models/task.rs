a29954de6d431952aa371d45aa00e06ebf92ed06
sigilmancer <em1ki@tuta.io>
2025-08-05 17:11:58 +0100

    Add files via upload

--- rust_api_two/src/models/task.rs
+++ rust_api_two/src/models/task.rs
@@ -1,1 +1,12 @@
+use serde::{Serialize, Deserialize};
+use uuid::Uuid;
+use chrono::{DateTime, Utc};
+use sqlx::FromRow;
 
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+pub struct Task {
+    pub id: Uuid,
+    pub title: String,
+    pub completed: bool,
+    pub created_at: DateTime<Utc>,
+}
